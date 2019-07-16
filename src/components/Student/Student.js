8f636bfdc3ed86b8361d5223c5e514b7ad5e96d5
Ali_Ahmadiavin <ahmadiavin@gmail.com>
2019-07-16 15:24:15 -0500

    initial commit - black diamond incomplete

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import axios from 'axios';
 export default class Student extends Component {
   constructor() {
-    super()
-
+    super();
+    this.state = {
+      studentInfo: {}
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3005/students/${this.props.match.params.id}`)
+      .then(res => {
+        this.setState({
+          studentInfo: res.data
+        });
+      });
+      // console.log("hey!!", this.props.grade)
   }
 
   render() {
     return (
+    
       <div className="box">
         <h1>Student</h1>
+        {
+          <h1>
+            {this.state.studentInfo.first_name} {this.state.studentInfo.last_name}
+          </h1>
+        }
+        {<h1>Grade: {this.state.studentInfo.grade}</h1>}
+        {<h1>Email: {this.state.studentInfo.email}</h1>}
       </div>
-    )
+    );
   }
 }
